a64d1bb8c1361245de2d915b740360e2fc61d091
Rozkata <cartmanbg23@abv.bg>
2020-04-23 02:45:54 -0700

    ADD: Client side and server side applications. TODO-pass data from server to frontend not upon button click, but directly-comments left on how to do it

---
+++ ServerSide/mainwindow.h
@@ -0,0 +1,46 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QMessageBox>
+#include <QMetaType>
+#include <QString>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+signals:
+    void newMessage(QString);
+
+private slots:
+    void newConnection();
+    void appendNewSocket(QTcpSocket* socket);
+
+    void readSocket();
+    void discardSocket();
+
+    void displayMessage(const QString& str);
+    void sendMessage(QTcpSocket* socket);
+
+    void on_pushButton_sendMessage_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    // declare our server
+    QTcpServer* m_server;
+    // declare list where we will store all sockets
+    QList<QTcpSocket*> socket_list;
+};
+#endif // MAINWINDOW_H
